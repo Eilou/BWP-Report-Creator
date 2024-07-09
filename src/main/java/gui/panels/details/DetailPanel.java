75ea54deda554b7d976c7b2a6bd8f37d103e0de8
Louie Brooks <louiewbrooks04@gmail.com>
2024-07-09 21:33:24 +0100

    moved the setup of the sections into the subclass as the number of columns and rows may be different for that

--- src/main/java/gui/panels/details/DetailPanel.java
+++ src/main/java/gui/panels/details/DetailPanel.java
@@ -29,33 +29,5 @@ public class DetailPanel extends JPanel {
         setBackground(Color.red);
         setBorder(border);
         setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-
-        int rows = 5;
-        int columns = 5;
-
-        gbc.gridwidth = 1;
-        gbc.gridheight = 1;
-        gbc.insets = new Insets(5,5,5,5);
-
-        JPanel[][] gridPanels = new JPanel[rows][columns];
-        for (int row = 0; row < gridPanels.length; row++) {
-            for (int column = 0; column < gridPanels[0].length; column++) {
-                gridPanels[row][column] = new JPanel();
-                gridPanels[row][column].setPreferredSize(new Dimension(50,50));
-                gridPanels[row][column].setBorder(new LineBorder(Color.BLACK));
-                gridPanels[row][column].setBackground(new Color(255,0,255));
-
-                gbc.fill = GridBagConstraints.BOTH; // stretch both horizontally and vertically
-                gbc.weightx = 1.0; // expand in both directions at equal rates
-                gbc.weighty = 1.0;
-
-                gbc.gridx = column;
-                gbc.gridy = row;
-
-                add(gridPanels[row][column], gbc);
-            }
-        }
-
     }
 }
--- src/main/java/gui/panels/details/DoorDetailsPanel.java
+++ src/main/java/gui/panels/details/DoorDetailsPanel.java
@@ -4,6 +4,8 @@ import enums.ReportState;
 import items.doors.*;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
 
 /**
  * Subclass to be used when creating a door report
@@ -14,6 +16,47 @@ public class DoorDetailsPanel extends DetailPanel{
     public DoorDetailsPanel(JPanel parentPanel, ReportState reportState, int count) {
         super(parentPanel, reportState, count);
         this.door = new Door(count);
+    }
+
+    /**
+     * Sets up the overall panel and the inner panel divisions ready for the different input forms
+     */
+    public void setup() {
+        super.setup();
+
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        int rows = 5;
+        int columns = 5;
+
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.insets = new Insets(5,5,5,5);
+
+        JPanel[][] gridPanels = new JPanel[rows][columns];
+        for (int row = 0; row < gridPanels.length; row++) {
+            for (int column = 0; column < gridPanels[0].length; column++) {
+                gridPanels[row][column] = new JPanel();
+                gridPanels[row][column].setPreferredSize(new Dimension(50,50));
+                gridPanels[row][column].setBorder(new LineBorder(Color.BLACK));
+                gridPanels[row][column].setBackground(new Color(255,0,255));
+
+                gbc.fill = GridBagConstraints.BOTH; // stretch both horizontally and vertically
+                gbc.weightx = 1.0; // expand in both directions at equal rates
+                gbc.weighty = 1.0;
+
+                gbc.gridx = column;
+                gbc.gridy = row;
+
+                add(gridPanels[row][column], gbc);
+            }
+        }
+
+        addForms();
+    }
+
+    public void addForms() {
+
     }
 
 }
