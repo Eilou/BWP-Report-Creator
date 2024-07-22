1800865d1e87c7613f37d00f433b3a11a445fd0e
Louie Brooks <louiewbrooks04@gmail.com>
2024-07-22 13:23:09 +0100

    set up the comboboxes to be populated, worth noting that comboboxes have a default blank space so it might not be necessary to add this into each combobox, but worth having it as an enum option as it means I don't have lots of null values

--- src/main/java/gui/panels/details/DoorDetailsPanel.java
+++ src/main/java/gui/panels/details/DoorDetailsPanel.java
@@ -7,48 +7,54 @@ import items.doors.*;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Subclass to be used when creating a door report
  */
-public class DoorDetailsPanel extends DetailPanel{
+public class DoorDetailsPanel extends DetailPanel {
     private Door door;
     private JPanel[][] gridPanels;
 
-    private JComboBox<String> floorComboBox;
-    private JComboBox<String> roomComboBox;
-    private JComboBox<String> wallConstructionComboBox;
-    private JComboBox<String> doorTypeComboBox;
-    private JComboBox<String> internalExternalComboBox;
-    private JComboBox<YesNoOptions> partMThresholdComboBox;
-    private JComboBox<String> fireRatingComboBox;
-    private JComboBox<YesNoOptions> glazedComboBox;
-    private JComboBox<String> leafSizeComboBox;
-    private JCheckBox doubleLeafSizeCheckbox;
+    private final JComboBox<String> floorComboBox = new JComboBox<>();
+    private final JComboBox<String> roomComboBox = new JComboBox<>();
+    private final JComboBox<String> wallConstructionComboBox = new JComboBox<>();
+    private final JComboBox<String> doorTypeComboBox = new JComboBox<>();
+    private final JComboBox<String> internalExternalComboBox = new JComboBox<>();
+    private final JComboBox<YesNoOptions> partMThresholdComboBox = new JComboBox<>();
+    private final JComboBox<String> fireRatingComboBox = new JComboBox<>();
+    private final JComboBox<YesNoOptions> glazedComboBox = new JComboBox<>();
+    private final JComboBox<String> leafSizeComboBox = new JComboBox<>();
+    private final JCheckBox doubleLeafSizeCheckbox = new JCheckBox();
 
-    private JComboBox<Integer> clearOpeningComboBox;
-    private JComboBox<YesNoOptions> entranceLevelComboBox;
-    private JComboBox<YesNoOptions> partMCompliantComboBox;
-    private JComboBox<YesNoOptions> additionalPlyLiningComboBox;
-    private JComboBox<String> structuralOpeningComboBox;
-    private JTextField structuralOpeningDetailsTextField;
+    private final JComboBox<Integer> clearOpeningComboBox = new JComboBox<>();
+    private final JComboBox<YesNoOptions> entranceLevelComboBox = new JComboBox<>();
+    private final JComboBox<YesNoOptions> partMCompliantComboBox = new JComboBox<>();
+    // this doesn't actually need to be a partM compliant box, but should be updates as a result
+    // of an if elseif else statement (look at the Excel sheet for reference)
+    private final JComboBox<YesNoOptions> additionalPlyLiningComboBox = new JComboBox<>();
+    private final JComboBox<String> structuralOpeningComboBox = new JComboBox<>();
+    // this should be a lookup table
+    private final JTextField structuralOpeningDetailsTextField = new JTextField();
 
-    private JTextField frameDetailsTextField;
-    private JTextField sillDetailsTextField;
-    private JTextField architraveTypeTextField;
+    private final JTextField frameDetailsTextField = new JTextField();
+    private final JTextField sillDetailsTextField = new JTextField();
+    private final JTextField architraveTypeTextField = new JTextField();
 
-    private JComboBox<String> hingesComboBox;
-    private JComboBox<YesNoOptions> latchComboBox;
-    private JComboBox<YesNoOptions> lockComboBox;
-    private JComboBox<YesNoOptions> handleComboBox;
-    private JTextField additionalNotesTextField;
+    private final JComboBox<String> hingesComboBox = new JComboBox<>();
+    private final JComboBox<YesNoOptions> latchComboBox = new JComboBox<>();
+    private final JComboBox<YesNoOptions> lockComboBox = new JComboBox<>();
+    private final JComboBox<YesNoOptions> handleComboBox = new JComboBox<>();
+    private final JTextField additionalNotesTextField = new JTextField();
 
     /**
      * Constructor called when the add item is called in door report state, instancing the
      * different inputs to then be added in later
-     * @param parentPanel
-     * @param reportState
-     * @param count
+     *
+     * @param parentPanel the panel which this panel is nested in
+     * @param reportState the current type of report being processed
+     * @param count       the door number
      */
     public DoorDetailsPanel(JPanel parentPanel, ReportState reportState, int count) {
         super(parentPanel, reportState, count);
@@ -57,34 +63,6 @@ public class DoorDetailsPanel extends DetailPanel{
         int columns = 5;
         gridPanels = new JPanel[rows][columns];
 
-        this.floorComboBox = new JComboBox<>();
-        this.roomComboBox = new JComboBox<>();
-        this.wallConstructionComboBox = new JComboBox<>();
-        this.doorTypeComboBox = new JComboBox<>();
-        this.internalExternalComboBox = new JComboBox<>();
-        this.partMThresholdComboBox = new JComboBox<>();
-        this.fireRatingComboBox = new JComboBox<>();
-        this.glazedComboBox = new JComboBox<>();
-        this.leafSizeComboBox = new JComboBox<>();
-        this.doubleLeafSizeCheckbox = new JCheckBox();
-
-        this.clearOpeningComboBox = new JComboBox<>();
-        this.entranceLevelComboBox = new JComboBox<>();
-        this.partMCompliantComboBox = new JComboBox<>();
-        this.additionalPlyLiningComboBox = new JComboBox<>();
-
-        this.structuralOpeningComboBox = new JComboBox<>();
-        this.structuralOpeningDetailsTextField = new JTextField();
-
-        this.frameDetailsTextField = new JTextField();
-        this.sillDetailsTextField = new JTextField();
-        this.architraveTypeTextField = new JTextField();
-
-        this.hingesComboBox = new JComboBox<>();
-        this.latchComboBox = new JComboBox<>();
-        this.lockComboBox = new JComboBox<>();
-        this.handleComboBox = new JComboBox<>();
-        this.additionalNotesTextField = new JTextField();
     }
 
     /**
@@ -97,15 +75,15 @@ public class DoorDetailsPanel extends DetailPanel{
 
         gbc.gridwidth = 1;
         gbc.gridheight = 1;
-        gbc.insets = new Insets(5,5,5,5);
+        gbc.insets = new Insets(5, 5, 5, 5);
 
         for (int row = 0; row < gridPanels.length; row++) {
             for (int column = 0; column < gridPanels[0].length; column++) {
                 gridPanels[row][column] = new JPanel();
                 gridPanels[row][column].setLayout(new BoxLayout(gridPanels[row][column], BoxLayout.PAGE_AXIS));
-                gridPanels[row][column].setPreferredSize(new Dimension(50,50));
+                gridPanels[row][column].setPreferredSize(new Dimension(50, 50));
                 gridPanels[row][column].setBorder(new LineBorder(Color.BLACK));
-                gridPanels[row][column].setBackground(new Color(255,0,255));
+                gridPanels[row][column].setBackground(new Color(255, 0, 255));
 
                 gbc.fill = GridBagConstraints.BOTH; // stretch both horizontally and vertically
                 gbc.weightx = 1.0; // expand in both directions at equal rates
@@ -118,6 +96,7 @@ public class DoorDetailsPanel extends DetailPanel{
             }
         }
 
+        populateComboBoxes();
         positionForms();
     }
 
@@ -157,6 +136,44 @@ public class DoorDetailsPanel extends DetailPanel{
         gridPanels[4][2].add(lockComboBox);
         gridPanels[4][3].add(handleComboBox);
         gridPanels[4][4].add(additionalNotesTextField);
+
+    }
+
+    /**
+     * Populates a given combobox with the contents of the array passed to it
+     *
+     * @param comboBox     the combobox to add items to
+     * @param optionsToAdd the options to add
+     * @param <T>          the type parameter ensuring that equal data types are held in the array list as
+     *                     are used in the combobox
+     */
+    public <T> void populateGivenComboBox(JComboBox<T> comboBox,
+                                       T[] optionsToAdd) {
+        DefaultComboBoxModel<T> model =
+                (DefaultComboBoxModel<T>) comboBox.getModel();
+        model.addAll(List.of(optionsToAdd));
+    }
+
+    /**
+     * Populates all the different comboboxes in the application
+     */
+    public void populateComboBoxes() {
+        populateGivenComboBox(floorComboBox, new String[]{"Ground Floor","First Floor","Second Floor","Third Floor","Lower Ground Floor","Upper Ground Floor","Mezzanine","Basement 1","Basement 2"});
+        populateGivenComboBox(wallConstructionComboBox, new String[]{"Masonry cavity wall","Timberframe","SIPS panel","100mm blockwork","140mm blockwork","215mm blockwork","89mm partition","100mm partition","140mm partition"});
+        populateGivenComboBox(internalExternalComboBox, new String[]{"Internal", "External [1]"});
+        populateGivenComboBox(partMThresholdComboBox, new YesNoOptions[]{YesNoOptions.BLANK, YesNoOptions.Y_2});
+        populateGivenComboBox(fireRatingComboBox, new String[]{"FD20 [3]","FD30 [3]","FD30-SC [3]","FD60 [3]"});
+        populateGivenComboBox(glazedComboBox, new YesNoOptions[]{YesNoOptions.BLANK, YesNoOptions.Y_4});
+        populateGivenComboBox(leafSizeComboBox, new String[]{"610","686","762","838","626","726", "826","926","2 x 610","2 x 686","2 x 762","2 x 838","2 x 626","2 x 726","2 x 826","2 x 926"});
+        populateGivenComboBox(entranceLevelComboBox, new YesNoOptions[]{YesNoOptions.BLANK, YesNoOptions.Y});
+        populateGivenComboBox(additionalPlyLiningComboBox, new YesNoOptions[]{YesNoOptions.BLANK, YesNoOptions.Y});
+        populateGivenComboBox(hingesComboBox, new String[]{"1 pair", "1 1/2 pair", "2 pair"}); //
+        // needs turning into the unicode values
+        populateGivenComboBox(latchComboBox, new YesNoOptions[]{YesNoOptions.BLANK, YesNoOptions.Y});
+        populateGivenComboBox(lockComboBox, new YesNoOptions[]{YesNoOptions.BLANK, YesNoOptions.Y, YesNoOptions.Y_5});
+        populateGivenComboBox(handleComboBox, new YesNoOptions[]{YesNoOptions.Y});
+
+
 
 
     }
