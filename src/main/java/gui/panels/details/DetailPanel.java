b2399259e4c7d8060ac03ea5189711d3465ddc74
Louie Brooks <louiewbrooks04@gmail.com>
2024-07-31 17:33:33 +0100

    omg after a good hour i have the border looking nice

--- src/main/java/gui/panels/details/DetailPanel.java
+++ src/main/java/gui/panels/details/DetailPanel.java
@@ -2,9 +2,11 @@ package gui.panels.details;
 
 import enums.ReportState;
 
+import javax.sound.sampled.Line;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
+import javax.swing.border.*;
 import java.awt.*;
 
 /**
@@ -14,16 +16,22 @@ import java.awt.*;
 public class DetailPanel extends JPanel {
 
     private JPanel parentPanel;
-    private TitledBorder border;
-   private int count;
+    private Border border;
+    private int count;
+    private String detailTitle;
 
     public DetailPanel(JPanel parentPanel, ReportState reportState, int count) {
         this.parentPanel = parentPanel;
         this.count = count;
         setPreferredSize(new Dimension(0, 200));
         setMaximumSize(new Dimension(Integer.MAX_VALUE, 200));
+        this.detailTitle = reportState.toString().charAt(0) + String.valueOf(count);
+//        border = new LineBorder(Color.black, 5);
+        int margin = 5;
+        border = BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(margin,margin,margin,
+                        margin),
+                BorderFactory.createLineBorder(Color.black, 5));
 
-        border = new TitledBorder(reportState.toString().charAt(0) + String.valueOf(count));
     }
     public void setup() {
         setBackground(Color.red);
